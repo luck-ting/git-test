ce12d7423c4a417ce6993ef9ffb731b7be03bb97
zhuting <2955238609@qq.com>
2021-07-05 19:14:49 +0800

    second coomit

--- src/main/java/com/zhuting/git/GitTest.java
+++ src/main/java/com/zhuting/git/GitTest.java
@@ -3,5 +3,6 @@ package com.zhuting.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
